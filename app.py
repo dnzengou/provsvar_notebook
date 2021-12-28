fc500c062b685421d5c5aa5c0950db11412b0ccc
dnzengou <desire.yavro@gmail.com>
2021-12-28 13:42:38 +0100

    update app.py with cards, datatable, bar and line charts

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
+from dash import dash_table
 import pandas as pd
 
 #data = pd.read_csv("https://raw.githubusercontent.com/dnzengou/provsvar_notebook/main/data/provsvar_sept_anonym_122021-Blad11_mal_full.csv")
-data = pd.read_csv('data/provsvar_sept_anonym_122021-Blad11_mal_full_clean.csv')
+data = pd.read_csv('data/provsvar_sept_anonym_122021-Blad11_mal_full_clean.csv') #with index
+#data = pd.read_csv('data/provsvar_sept_anonym_full_clean_indexed.csv') # with 
 
 # data cleaning
 # renaming the df column names to lowercase
@@ -50,62 +53,112 @@ app.layout = html.Div(
             className="header",
         ),
         html.Div(
+            dash_table.DataTable(
+                id='table-paging-with-graph',
+                columns=[
+                    {"name": i, "id": i} for i in sorted(data.columns)
+                ],
+                page_current=0,
+                page_size=20,
+                page_action='custom',
+
+                filter_action='custom',
+                filter_query='',
+
+                sort_action='custom',
+                sort_mode='multi',
+                sort_by=[]
+            ),
+            style={'height': 750, 'overflowY': 'scroll'},
+            className='six columns'
+        ),
+        html.Div(
+            id='table-paging-with-graph-container',
+            className="five columns"
+        ),
+        
+        
+        html.Div(
             children=[
                 html.Div(
                     children=dcc.Graph(
-                        id="varde1-chart",
+                        id="varde1-barchart",
                         config={"displayModeBar": False},
                         figure={
                             "data": [
                                 {
-                                    "x": data["provsvarsdate"],
+                                    "x": data["personnummer"],
                                     "y": data["varde1"],
-                                    "type": "lines",
-                                    "hovertemplate": "$%{y:.2f}"
-                                                     "<extra></extra>",
+                                    "type": "bar",
+                                    "marker": {"color": "#0074D9"},
                                 },
                             ],
                             "layout": {
                                 "title": {
-                                    "text": "Värde 1 över tiden",
-                                    "x": 0.05,
-                                    "xanchor": "left",
-                                },
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {
-                                    "tickprefix": "$",
-                                    "fixedrange": True,
-                                },
-                                "colorway": ["#17B897"],
-                            },
-                        },
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="varde2-chart",
-                        config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": data["provsvarsdate"],
-                                    "y": data["varde2"],
-                                    "type": "lines",
-                                },
-                            ],
-                            "layout": {
-                                "title": {
-                                    "text": "Värde 2 över tiden",
+                                    "text": "Värde 1 av personnummer",
                                     "x": 0.05,
                                     "xanchor": "left",
                                 },
                                 "xaxis": {"fixedrange": True},
                                 "yaxis": {"fixedrange": True},
-                                "colorway": ["#E12D39"],
+                                "colorway": ["#0074D9"],
                             },
                         },
-                    ),
+                    ),                    
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="varde2-barchart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["personnummer"],
+                                    "y": data["varde2"],
+                                    "type": "bar",
+                                    "marker": {"color": "#0074D9"},
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Värde 2 av personnummer",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {"fixedrange": True},
+                                "colorway": ["#0074D9"],
+                            },
+                        },
+                    ),                    
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="varde3-barchart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["personnummer"],
+                                    "y": data["varde3"],
+                                    "type": "bar",
+                                    "marker": {"color": "#0074D9"},
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Värde 3 av personnummer",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {"fixedrange": True},
+                                "colorway": ["#0074D9"],
+                            },
+                        },
+                    ),                    
                     className="card",
                 ),
                 html.Div(
@@ -118,6 +171,8 @@ app.layout = html.Div(
                                     "x": data["provsvarsdate"],
                                     "y": data["varde3"],
                                     "type": "lines",
+                                    "hovertemplate": "%{y:.2f}"
+                                                     "<extra></extra>",
                                 },
                             ],
                             "layout": {
@@ -127,13 +182,16 @@ app.layout = html.Div(
                                     "xanchor": "left",
                                 },
                                 "xaxis": {"fixedrange": True},
-                                "yaxis": {"fixedrange": True},
-                                "colorway": ["#5B5B5B"],
+                                "yaxis": {
+                                    "tickprefix": " ",
+                                    "fixedrange": True,
+                                },
+                                "colorway": ["#8AD868"],
                             },
                         },
                     ),
                     className="card",
-                ),
+                ),                    
                 html.Div(
                     children=dcc.Graph(
                         id="varde4-chart",
@@ -154,7 +212,7 @@ app.layout = html.Div(
                                 },
                                 "xaxis": {"fixedrange": True},
                                 "yaxis": {"fixedrange": True},
-                                "colorway": ["##A64D79"],
+                                "colorway": ["#8AD868"],
                             },
                         },
                     ),
@@ -191,6 +249,113 @@ app.layout = html.Div(
         ),
     ]
 )
+
+
+
+
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
+
+
+def split_filter_part(filter_part):
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+
+                # word operators need spaces after them in the filter string,
+                # but we don't want these later
+                return name, operator_type[0].strip(), value
+
+    return [None] * 3
+
+
+@app.callback(
+    Output('table-paging-with-graph', "data"),
+    Input('table-paging-with-graph', "page_current"),
+    Input('table-paging-with-graph', "page_size"),
+    Input('table-paging-with-graph', "sort_by"),
+    Input('table-paging-with-graph', "filter_query"))
+def update_table(page_current, page_size, sort_by, filter):
+    filtering_expressions = filter.split(' && ')
+    dff = data
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            # these operators match pandas series operator method names
+            dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dff = dff.loc[dff[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            # this is a simplification of the front-end filtering logic,
+            # only works with complete fields in standard format
+            dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+
+    if len(sort_by):
+        dff = dff.sort_values(
+            [col['column_id'] for col in sort_by],
+            ascending=[
+                col['direction'] == 'asc'
+                for col in sort_by
+            ],
+            inplace=False
+        )
+
+    return dff.iloc[
+        page_current*page_size: (page_current + 1)*page_size
+    ].to_dict('records')
+
+
+#@app.callback(
+#    Output('table-paging-with-graph-container', "children"),
+#    Input('table-paging-with-graph', "data"))
+#def update_graph(rows):
+#    dff = pd.DataFrame(rows)
+#    return html.Div(
+#        [
+#            dcc.Graph(
+#                id=column,
+#                figure={
+#                    "data": [
+#                        {
+                            #"x": dff["provsvarsdate"],
+#                            "x": dff["personnummer"],
+#                            "y": dff[column] if column in dff else [],
+#                            "type": "bar",
+#                            "marker": {"color": "#0074D9"},
+#                        }
+#                    ],
+#                    "layout": {
+#                        "xaxis": {"automargin": True},
+#                        "yaxis": {"automargin": True},
+#                        "height": 250,
+#                        "margin": {"t": 10, "l": 10, "r": 10},
+#                    },
+#                },
+#            )
+#            for column in ["varde1", "varde2", "varde3"]
+#        ]
+#    )
+
+
 
 
 if __name__ == "__main__":
